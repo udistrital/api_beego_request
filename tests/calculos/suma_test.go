b260fb2a5b7b213e6331432ac79be5da2e36c593
jjvargass <jotavargas89@gmail.com>
2019-05-24 16:49:19 -0500

    paso de parametros en test

--- tests/calculos/suma_test.go
+++ tests/calculos/suma_test.go
@@ -1,10 +1,23 @@
-package test
+package calculos
 
 import (
+	"flag"
+	"os"
 	"testing"
 
 	"github.com/udistrital/api_beego_request/calculos"
 )
+
+var parameters struct {
+	Endpoint_1 string
+}
+
+func TestMain(m *testing.M) {
+
+	parameters.Endpoint_1 = os.Getenv("Endpoint1")
+	flag.Parse()
+	os.Exit(m.Run())
+}
 
 func TestSuma(t *testing.T) {
 	valor := calculos.Suma(2, 2)
@@ -14,4 +27,10 @@ func TestSuma(t *testing.T) {
 	} else {
 		t.Log("TestSuma Finalizado Correctamente (OK)")
 	}
+}
+
+func TestEndPointSuma(t *testing.T) {
+	t.Log("AAAAAAAAAAAAAAAA")
+	t.Log(parameters.Endpoint_1)
+	t.Log("AAAAAAAAAAAAAAAA")
 }
--- tests/controllers/usuario_test.go
+++ tests/controllers/usuario_test.go
@@ -1,10 +1,23 @@
-package test
+package controllers
 
 import (
+	"flag"
+	"os"
 	"testing"
 
 	"github.com/udistrital/api_beego_request/controllers"
 )
+
+var parameters struct {
+	Endpoint_2 string
+}
+
+func TestMain(m *testing.M) {
+
+	parameters.Endpoint_2 = os.Getenv("Endpoint2")
+	flag.Parse()
+	os.Exit(m.Run())
+}
 
 func TestResta(t *testing.T) {
 	valor := controllers.Resta(4, 2)
@@ -14,4 +27,10 @@ func TestResta(t *testing.T) {
 	} else {
 		t.Log("TestResta Finalizado Correctamente (OK)")
 	}
+}
+
+func TestEndPointResta(t *testing.T) {
+	t.Log("AAAAAAAAAAAAAAAA")
+	t.Log(parameters.Endpoint_2)
+	t.Log("AAAAAAAAAAAAAAAA")
 }
